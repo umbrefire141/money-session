fa4f58f6e9105a50aff6a409bd79ce3770529178
Matvey <matvejaksentev@gmail.com>
2024-11-25 20:18:27 +0700

    Changes

--- src/routing/Home/blocks/Advantages/Card/Card.tsx
+++ src/routing/Home/blocks/Advantages/Card/Card.tsx
@@ -1,6 +1,7 @@
 import Typography from '@/components/shared/Typography/Typography';
 import clsx from 'clsx';
 import Image from 'next/image';
+import Link from 'next/link';
 import { ICardProps } from './Card.interface';
 import styles from './Card.module.css';
 
@@ -25,35 +26,37 @@ const Card = ({ img, title, text }: ICardProps) => {
 				{text ? (
 					<Typography color="gray">{text}</Typography>
 				) : (
-					<svg
-						width="47"
-						height="46"
-						viewBox="0 0 47 46"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							d="M23.988 42.1668C34.5734 42.1668 43.1546 33.5856 43.1546 23.0002C43.1546 12.4147 34.5734 3.8335 23.988 3.8335C13.4025 3.8335 4.82129 12.4147 4.82129 23.0002C4.82129 33.5856 13.4025 42.1668 23.988 42.1668Z"
-							stroke="white"
-							strokeWidth="2"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-						/>
-						<path
-							d="M23.9878 30.6668L31.6545 23.0002L23.9878 15.3335"
-							stroke="white"
-							strokeWidth="2"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-						/>
-						<path
-							d="M16.3213 23H31.6546"
-							stroke="white"
-							strokeWidth="2"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-						/>
-					</svg>
+					<Link href="#feedback">
+						<svg
+							width="47"
+							height="46"
+							viewBox="0 0 47 46"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M23.988 42.1668C34.5734 42.1668 43.1546 33.5856 43.1546 23.0002C43.1546 12.4147 34.5734 3.8335 23.988 3.8335C13.4025 3.8335 4.82129 12.4147 4.82129 23.0002C4.82129 33.5856 13.4025 42.1668 23.988 42.1668Z"
+								stroke="white"
+								strokeWidth="2"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							/>
+							<path
+								d="M23.9878 30.6668L31.6545 23.0002L23.9878 15.3335"
+								stroke="white"
+								strokeWidth="2"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							/>
+							<path
+								d="M16.3213 23H31.6546"
+								stroke="white"
+								strokeWidth="2"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							/>
+						</svg>
+					</Link>
 				)}
 			</div>
 			<Image
